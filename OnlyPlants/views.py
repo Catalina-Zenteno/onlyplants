bbdc51a6b03b03111dfd7742f0818276ea378b07
catazen15 <catalinazenteno04@gmail.com>
2023-08-26 18:11:06 -0400

    shortcut

--- OnlyPlants/views.py
+++ OnlyPlants/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render
+from django.template import loader
 
 #Esto es una vista
 def bienvenida(request):
@@ -10,9 +11,4 @@ def inicia_sesión(request):
     return HttpResponse("<p style= 'color: green;'>Inicia sesión</p>")
 
 def intrucciones(request):
-    plantillaExterna= open("C:/Users/Usuario/OnlyPlants/onlyplants-1/nombreapp/templates/intrucciones.html")
-    template= Template(plantillaExterna.read())
-    plantillaExterna.close()
-    contexto= Context()
-    documento= template.render(contexto)
-    return HttpResponse(documento)
+    return render(request, "intrucciones.html")
