30c230c8b3ee431d1c2f4143b3733b61d5664dfc
catazen15 <catalinazenteno04@gmail.com>
2023-08-31 19:44:02 -0400

    ya muestra imagen  :D

--- OnlyPlants/settings.py
+++ OnlyPlants/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import django.contrib.staticfiles
+from os.path import join
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
